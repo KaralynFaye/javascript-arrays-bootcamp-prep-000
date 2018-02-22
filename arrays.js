c02246f073da28bdbe4bfc0ba8d344f49262a93b
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-22 01:37:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 function addElementToBeginningOfArray(array, element) {
-  return [element,...array]
+  return [element,...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.push(element)
+  return array.push(element);
 }
 
 function accessElementInArray(array,i){
@@ -14,3 +14,17 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
   return array;
 }
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1);
+}
+
